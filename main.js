a9290481ac4ed5fa183fabf61f08e1fa549a29f7
renildomarcio <krsolucoesweb@gmail.com>
2024-09-25 11:28:00 -0300

    Update 1.0.7

--- main.js
+++ main.js
@@ -33,80 +33,50 @@ app.on('activate', () => {
 
 // Manipuladores de atualização
 function handleUpdateChecking() {
-  log.log('Checking for updates.');
-            mainWindow.webContents.executeJavaScript(`
-                Swal.fire({
-                    title: 'Procurando por atualizações...',
-                    allowOutsideClick: false,
-                    onBeforeOpen: () => Swal.showLoading()
-                });
-            `);
-}
+    log.log('Checking for updates.');
+  }
+    
+  function handleUpdateAvailable(info) {
+    log.log('Update available.');
+  }
+    
+  function handleDownloadProgress(progressObj) {
+    const message = `Downloading update. Speed: ${progressObj.bytesPerSecond} - ${~~progressObj.percent}% [${progressObj.transferred}/${progressObj.total}]`;
+    log.log(message);
+    const swalMessage = `Swal.fire({
+      title: 'Baixando atualização',
+      html: '${message}',
+      allowOutsideClick: false,
+      onBeforeOpen: () => Swal.showLoading()
+    });`;
   
-function handleUpdateAvailable(info) {
-  log.log('Update available.');
-            mainWindow.webContents.executeJavaScript(`
-                Swal.fire({
-                    title: 'Atualização disponível',
-                    text: 'Uma nova atualização está disponível!',
-                    icon: 'info',
-                    allowOutsideClick: false
-                });
-            `);
-}
+    mainWindow.webContents.executeJavaScript(swalMessage);
+  }
+    
+  function handleUpdateError(err) {
+    log.log(`Update check failed: ${err.toString()}`);
+  }
+    
+  function handleUpdateNotAvailable(info) {
+    log.log(`Não há atualizações disponíveis para o launcher.`);
+  }
+    
+  function handleUpdateDownloaded(info) {
+    const swalMessage = `Swal.fire({
+      title: 'Reiniciando o aplicativo',
+      html: 'Aguente firme, reiniciando o aplicativo para atualização!',
+      allowOutsideClick: false,
+      onBeforeOpen: () => Swal.showLoading()
+    });`;
+    
+    mainWindow.webContents.executeJavaScript(swalMessage);
+    autoUpdater.quitAndInstall();
+  }
+    
+  autoUpdater.on('checking-for-update', handleUpdateChecking);
+  autoUpdater.on('update-available', handleUpdateAvailable);
+  autoUpdater.on('download-progress', handleDownloadProgress);
+  autoUpdater.on('error', handleUpdateError);
+  autoUpdater.on('update-not-available', handleUpdateNotAvailable);
+  autoUpdater.on('update-downloaded', handleUpdateDownloaded);
   
-function handleDownloadProgress(progressObj) {
-  const message = `Downloading update. Speed: ${progressObj.bytesPerSecond} - ${~~progressObj.percent}% [${progressObj.transferred}/${progressObj.total}]`;
-  log.log(message);
-  const swalMessage = `Swal.fire({
-    title: 'Baixando atualização',
-    html: '${message}',
-    allowOutsideClick: false,
-    onBeforeOpen: () => Swal.showLoading()
-  });`;
-
-  mainWindow.webContents.executeJavaScript(swalMessage);
-}
-  
-function handleUpdateError(err) {
-  log.log(`Update check failed: ${err.toString()}`);
-            mainWindow.webContents.executeJavaScript(`
-                Swal.fire({
-                    title: 'Erro ao buscar atualização',
-                    text: '${err.toString()}',
-                    icon: 'error',
-                    allowOutsideClick: true
-                });
-            `);
-}
-  
-function handleUpdateNotAvailable(info) {
-  log.log('Não há atualizações disponíveis para o launcher.');
-            mainWindow.webContents.executeJavaScript(`
-                Swal.fire({
-                    title: 'Nenhuma atualização disponível',
-                    text: 'Você já está utilizando a versão mais recente.',
-                    icon: 'info',
-                    allowOutsideClick: true
-                });
-            `);
-}
-  
-function handleUpdateDownloaded(info) {
-  const swalMessage = `Swal.fire({
-    title: 'Reiniciando o aplicativo',
-    html: 'Aguente firme, reiniciando o aplicativo para atualização!',
-    allowOutsideClick: false,
-    onBeforeOpen: () => Swal.showLoading()
-  });`;
-  
-  mainWindow.webContents.executeJavaScript(swalMessage);
-  autoUpdater.quitAndInstall();
-}
-  
-autoUpdater.on('checking-for-update', handleUpdateChecking);
-autoUpdater.on('update-available', handleUpdateAvailable);
-autoUpdater.on('download-progress', handleDownloadProgress);
-autoUpdater.on('error', handleUpdateError);
-autoUpdater.on('update-not-available', handleUpdateNotAvailable);
-autoUpdater.on('update-downloaded', handleUpdateDownloaded);
